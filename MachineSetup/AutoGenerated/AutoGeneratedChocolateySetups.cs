a300e6c88f4eaa1dca841a428281d7eda2d8cc38
Manuel Maier <mjmaier@gmx.de>
2017-09-09 20:10:03 +0200

    Add some more setups according to the TODO.

--- MachineSetup/AutoGenerated/AutoGeneratedChocolateySetups.cs
+++ MachineSetup/AutoGenerated/AutoGeneratedChocolateySetups.cs
@@ -90,5 +90,104 @@ namespace MachineSetup
             context.ExecuteChocolatey("install", "beyondcompare");
         }
     }
+
+    [Setup("Dropbox")]
+    public class DropboxSetup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "dropbox");
+        }
+    }
+
+    [Setup("Adobe Reader DC")]
+    public class AdobeReaderDCSetup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "adobereader");
+        }
+    }
+
+    [Setup("Foobar 2000")]
+    public class Foobar2000Setup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "foobar2000");
+        }
+    }
+
+    [Setup("Tortoise SVN")]
+    public class TortoiseSvnSetup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "tortoisesvn");
+        }
+    }
+
+    [Setup("OpenVPN")]
+    public class OpenVpnSetup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "openvpn");
+        }
+    }
+
+    [Setup("Postman")]
+    public class PostmanSetup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "postman");
+        }
+    }
+
+    [Setup("VirtualBox")]
+    public class VirtualBoxSetup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "virtualbox");
+        }
+    }
+
+    [Setup("Telegram")]
+    public class TelegramSetup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "telegram.install");
+        }
+    }
+
+    [Setup("TeamSpeak 3")]
+    public class TeamSpeak3Setup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "teamspeak");
+        }
+    }
+
+    [Setup("PotPlayer")]
+    public class PotPlayerSetup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "potplayer");
+        }
+    }
+
+    [Setup("LibreOffice")]
+    public class LibreOfficeSetup : ISetup
+    {
+        public void Run(SetupContext context)
+        {
+            context.ExecuteChocolatey("install", "libreoffice");
+        }
+    }
 }
 
--- TODO.md
+++ TODO.md
@@ -1,17 +1,5 @@
-* Dropbox
-* Adobe Acrobat Reader
-* foobar2000
-* LibreOffice
-* TortoiseSVN
 * PowerVR TextureTool (will be tricky because we only need one specific thing from a collective installer)
-* PotPlayer
-* Java?
-* OpenVPN
-* Postman
 * Mailbird
-* VirtualBox
-* TeamSpeak 3
-* Telegram
 
 * Visual Studio settings
 * Visual Studio MSVC toolchain?
